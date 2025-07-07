78739b7f08a3e8cec24d8152dc3f5a8c4c6ea4ef
Ranveer-code1 <ranveer.arora112@education.nsw.gov.au>
2025-07-07 16:47:39 +1000

    Add files via upload

---
+++ print.py
@@ -0,0 +1,7 @@
+print("Welcome to Python")
+print('welcome to python')
+#this is a comment
+"""this is a multi line comment"""
+print("Ranveer \nArora")
+print("I live in",end=" ")
+print("Australia")
---
+++ welcome.py
@@ -0,0 +1,6 @@
+a="Ryan"
+b="12"
+print("once there was a boy called",a,". His age was",b,".")
+c=5
+d=2
+print(c+c-d)
