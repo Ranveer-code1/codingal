6ca32fbe965c854c19729d3e2a538e8150f857be
Ranveer-code1 <ranveer.arora112@education.nsw.gov.au>
2025-08-14 15:32:06 +1000

    Add files via upload

---
+++ afp44.py
@@ -0,0 +1,15 @@
+class parrot:
+    name="T.O.M"
+    def __init__(self,age):
+        self.age=age
+obj=parrot("2")
+print("My age is",obj.age,"years")
+print("My code name is ",obj.name)
+class parrot2:
+    name="J.E.R.R.Y"
+    def __init__(self,age):
+        self.age=age
+
+obj1=parrot2("2")
+print("My age is",obj1.age,"months")
+print("My code name is ",obj1.name)
