a37595eab9f00ef95c922896682a5ac59d89640e
Ranveer-code1 <ranveer.arora112@education.nsw.gov.au>
2025-09-22 16:30:34 +1000

    Update afp49.py

---
+++ afp49.py
@@ -0,0 +1,8 @@
+from tkinter import *
+window=Tk()
+window.title("Rock,paper,scisors!")
+window.geometry("500x500")
+window.configure(bg="black")
+btn=Button(text="Enter")
+btn.pack()
+window.mainloop()
