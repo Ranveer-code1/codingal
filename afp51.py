9d6e21e1ff5ff2ddf4e0a05aa1880a73a1b724f6
Ranveer-code1 <ranveer.arora112@education.nsw.gov.au>
2025-09-24 19:54:22 +1000

    Add files via upload

---
+++ afp51.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "))
+m=int(input("Enter a number: "))
+print(n*m)
+
+def fun():
+    multiply=0
+    for n in range(1,m+1):
+        
+        multiply=m*n
+    return multiply
+print(fun())
