43f492fecb4d4232d3ad0073c3faa28cfec25d17
Ranveer-code1 <ranveer.arora112@education.nsw.gov.au>
2026-01-05 17:03:43 +1100

    Update activity1.sql

---
+++ activity1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE suplier1(
+snumber text primary key,
+sname text,
+sage int);
+insert into suplier1(snumber,sname,sage) VALUES
+("1","Mason",12),
+("2","Ryan",12),
+("3","Campbell",12),
+("4","Quinlan",12),
+("5","Ranveer",12);
+SELECT * FROM suplier1;
